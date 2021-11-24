b0e38e7a41377a0824ae31ed516faa940f1cacb4
Lenovo-hzn <344242221@qq.com>
2021-11-24 20:45:58 +0800

    21

---
+++ 21.py
@@ -0,0 +1,39 @@
+# 合并两个有序链表
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # 递归
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+    def diedai(self, l1: ListNode, l2: ListNode) -> ListNode:
+        prehead = ListNode(-1)
+
+        prev = prehead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+
+        prev.next = l1 if l1 is not None else l2
+
+        return prehead.next

7226afb6e63494ba08a6b73b68b800f3b89437a5
Lenovo-hzn <344242221@qq.com>
2022-06-19 22:16:20 +0800

    28

--- 28.py
+++ 28.py
@@ -1,21 +1,36 @@
 def strStr(haystack: str, needle: str) -> int:
-    n = len(haystack)
-    m = len(needle)
+    def getnext(a, needle):
+        next = ['' for i in range(a)]
 
-    i = 0
-    while i+m <= n:
-        flag = True
-        for j in range(m):
-            if haystack[i+j] != needle[j]:
-                flag = False
-                break
+        k = -1
+        next[0] = k
+        for i in range(1, a):
+            while k > -1 and needle[k + 1] != needle[i]:
+                k = next[k]
+            if needle[k + 1] == needle[i]:
+                k += 1
+            next[i] = k
+        return next
 
-        if flag:
-            return i
-        i += 1
+    a = len(needle)
+    b = len(haystack)
 
+    # 如果要匹配空串
+    if a == 0:
+        return 0
+
+    next = getnext(a, needle)
+    p = -1
+    for j in range(b):
+        while p >= 0 and needle[p + 1] != haystack[j]:
+            p = next[p]
+        if needle[p + 1] == haystack[j]:
+            p += 1
+        if p == a - 1:
+            return j - a + 1
     return -1
+
 
 haystack = "hello"
 needle = "ll"
-strStr(haystack, needle)
+print(strStr(haystack, needle))

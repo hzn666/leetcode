e87dd1b290828c76aa01a4158a3adb58dfb2e52f
hzn <344242221@qq.com>
2022-07-24 23:00:39 +0800

    236

---
+++ 236.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    if not root or root == p or root == q:
+        return root
+
+    left = self.lowestCommonAncestor(root.left, p, q)
+    right = self.lowestCommonAncestor(root.right, p, q)
+
+    if left and right:
+        return root
+    if left:
+        return left
+
+    return right
